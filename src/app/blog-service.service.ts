716593444913fdebcc5637851518b825e934562d
mazharku <mazhar.shapnil@gmail.com>
2021-05-11 01:56:12 +0600

    procedural api connected

--- src/app/blog-service.service.ts
+++ src/app/blog-service.service.ts
@@ -6,8 +6,8 @@ import { HttpClient,HttpParams  } from '@angular/common/http';
 })
 export class BlogServiceService {
   
-  baseURL: string = "https://microblog-reactive.herokuapp.com"
-//baseURL: string = "https://microblog-ap.herokuapp.com"
+  //baseURL: string = "https://microblog-reactive.herokuapp.com"
+baseURL: string = "https://microblog-ap.herokuapp.com"
 //baseURL: string = "http://localhost:8082"
 constructor(private http: HttpClient) { }
 public makePost(post: any) {
